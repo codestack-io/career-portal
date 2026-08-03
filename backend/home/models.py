d11410305c1870af1ee947d2d98cd74da8c8b8bd
sahaantara567 <sahaantara567@gmail.com>
2026-08-03 22:22:40 +0600

    Add HeroBanner model and configure Django admin

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class HeroBanner(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.TextField()
+    button_text = models.CharField(max_length=100)
+    button_link = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
