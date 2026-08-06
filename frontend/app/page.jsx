ffc27179e76c148f875d1ceb10e2468218292d7b
sahaantara567 <sahaantara567@gmail.com>
2026-08-06 21:44:31 +0600

    Add statistics,destination section

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -5,15 +5,23 @@ import Services from "@/components/Services/Services";
 import Footer from "@/components/Footer/Footer";
 import FAQ from "@/components/FAQ/FAQ";
 import Testimonials from "@/components/Testimonials/Testimonials";
-
+import WhyChooseUs from "@/components/WhyChooseUs/WhyChooseUs";
+import Blogs from "@/components/Blogs/Blogs";
+import StudyDestinations from "@/components/StudyDestinations/StudyDestinations";
+import Statistics from "@/components/Statistics/Statistics";
 import {
   getHero,
   getAbout,
   getServices,
   getFooter,
   getFAQs,
-  getTestimonials
+  getTestimonials,
+  getWhyChooseUs,
+  getBlogs,
+  getStudyDestinations,
+  getStatistics,
 } from "@/lib/api";
+
 
 export default async function Home() {
   const hero = (await getHero())[0];
@@ -22,16 +30,24 @@ export default async function Home() {
   const footer = (await getFooter())[0];
   const faqs = await getFAQs();
   const testimonials = await getTestimonials();
-
+  const whyChooseUs = await getWhyChooseUs();
+  const blogs = await getBlogs();
+  const studyDestinations = await getStudyDestinations();
+  const statistics = await getStatistics();
   return (
     <>
       <Navbar />
       <Hero hero={hero} />
       <About about={about} />
       <Services services={services} />
+      <StudyDestinations destinations={studyDestinations} />
+      <WhyChooseUs features={whyChooseUs} />
+      <Statistics statistics={statistics} />
       <Testimonials testimonials={testimonials} />
+      <Blogs blogs={blogs} />
       <FAQ faqs={faqs} />
       <Footer footer={footer} />
+      
       
       
       
