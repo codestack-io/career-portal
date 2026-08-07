66c4089a1178689929fd3541ffe2b400a35bbe38
sahaantara567 <sahaantara567@gmail.com>
2026-08-07 12:36:38 +0600

    Configure for deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // Required for static export if using Next.js Image component
+  },
 };
 
 export default nextConfig;
